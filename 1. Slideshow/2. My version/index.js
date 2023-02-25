84fc5c976aa0bcb071647facebe869dcd6a4162a
Marat <80181316+MaratFa@users.noreply.github.com>
2023-02-25 23:18:07 +0300

    Downloaded from a local storage

--- 1. Slideshow/2. My version/index.js
+++ 1. Slideshow/2. My version/index.js
@@ -1,38 +1,48 @@
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
-const images = document.querySelector('.carousel').children;
+// Function for buttons
+const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
+const images = document.querySelector(".carousel").children;
 const totalImages = images.length;
 let index = 0;
 
-prev.addEventListener('click', () => {
-  nextImage('next');
-})
-
-next.addEventListener('click', () => {
-  nextImage('prev');
-})
-
-function nextImage(direction) {
-  if(direction == 'next') {
+function nextImage(direction) { 
+  if (direction == 'next') {
     index++;
-    if(index == totalImages) {
-      index = 0;
-    }
-  } else {
-    if(index == 0) {
-      index = totalImages - 1;
-    } else {
-      index--;
-    }
   }
-
+  if (direction == 'prev') {
+    index--;
+  }
+  if (index < 0) {
+    index = images.length + index;
+  }
+  if (index > images.length - 1) {
+    index = 0;
+  }  
   for(let i = 0; i < images.length; i++) {
     images[i].classList.remove('main');
   }
   images[index].classList.add('main');
 }
 
-// Dot buttons function
-function dotFunction() {
-    let dots = getElementsByClassName("dot");
+prev.addEventListener('click', () => {
+  nextImage('prev');
+})
+
+next.addEventListener('click', () => {
+  nextImage('next');
+})
+
+// Function for dots
+const dots = document.querySelector(".dots").children;
+
+function displayImage(currentDot) {
+  for(let i = 0; i < images.length; i++) {
+    images[i].classList.remove('main');
+  }
+  for(let j = 0; j < dots.length; j++) {
+    dots[j].classList.remove('dotActive');
+    dots[j].classList.add('dot');
+  }
+  images[currentDot].classList.add('main');  
+  dots[currentDot].classList.add('dotActive');
 }
